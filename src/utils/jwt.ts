c91b7e88e59392eacb368ec7913e6bc0278eb74c
SBAhsan <ahsangustavo90@gmail.com>
2026-08-03 15:09:02 +0600

    created auth middleware for rbac and catchAsync, jwt, sendResonse functions in utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,33 +2,35 @@ import { JwtPayload, SignOptions } from "jsonwebtoken";
 import jwt from "jsonwebtoken";
 import config from "../config";
 
-const createToken = (jwtPayload: JwtPayload, secret: string, expiresIn: SignOptions) => {
-    const token = jwt.sign(jwtPayload, secret, {expiresIn} as SignOptions);
+const createToken = (
+  jwtPayload: JwtPayload,
+  secret: string,
+  expiresIn: SignOptions,
+) => {
+  const token = jwt.sign(jwtPayload, secret, { expiresIn } as SignOptions);
 
-    return token;
+  return token;
 };
-
 
 const verifyToken = (token: string, secret: string) => {
-    try {
-        const verifyToken = jwt.verify(token, secret);
+  try {
+    const verifyToken = jwt.verify(token, secret);
 
-        return {
-            success: true,
-            data: verifyToken
-        }
-    } catch (error: any) {
-        console.log("Token verification failed", error);
+    return {
+      success: true,
+      data: verifyToken,
+    };
+  } catch (error: any) {
+    console.log("Token verification failed", error);
 
-        return {
-            success: false,
-            error: error.message
-        }
-    }
+    return {
+      success: false,
+      error: error.message,
+    };
+  }
 };
 
-
 export const jwtUtils = {
-    createToken,
-    verifyToken
-}
+  createToken,
+  verifyToken,
+};
